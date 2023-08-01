e4cb75824152ee9d47e9649fbf41d299cf419c4b
João Tavares <45982943+joaotavaresti@users.noreply.github.com>
2023-08-01 19:01:55 +0000

    Aiming to contribute to the movie-rental-api project, I followed the bottom-up analysis strategy, segmenting it into five steps.
    1) I understood the database schema and structure.
    Regarding this context, I noted that table users, movies, rentals, and favorite_movies were adequately defined and pre-populated by the seed file. Data were not provided in the seeds file except for the rentals table.
    
    2) I studied the models layer to find any bug or critical issue to be approached. I did not note any problematic points. Hence, I did not suggest improvements on this part of the project.
    
    3) After the revision of models dir, I followed the investigation of the controllers' layer.
    
    3.1 - In the class movies_controller at rent method, the original code does not correctly validate the available_copies attribute decrementation. It can cause an inconsistent negative value for this attribute. Each time that rent method is called, the available _copies decrease with no control for zero or negative value.
    >> Action: The refactored code makes this operation works accurately. With no more zero or negative values. A JSON is responded to with a customized message.
    
    3.2 - In movies_controller, the rent method was not controlling the repeated requests for the same user_id and movie_id.
    >> Action: The refactored code makes this operation works accurately. With no more create tentatives for the same user_id AND movie_id. A JSON is responded to with a customized message.
    
    3.3 - In movies_controller, I implemented an addition validation whether table movies has eventually no registers.
    >> Action: The refactored code with a JSON customized response message for this situation.
    
    3.4 - In movies_controller, for all methods, a user_id OR movie_id not found control	was not implemented.
    >> Action: The refactored code with a JSON customized response message for this situation.
    
    4) Recommendation service could be refactored aiming the code optimization
    >> Action: I refactored the recommendation_engine service aiming for its qualification, improving some internal naming conventions and operations.
    
    Thanks.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,26 +1,57 @@
 class MoviesController < ApplicationController
   def index
     @movies = Movie.all
-    render json: @movies
+
+    if @movies.empty?
+      render json: { message: "No movies found." }, status: :not_found
+    else
+      render json: @movies
+    end
   end
 
   def recommendations
-    favorite_movies = User.find(params[:user_id]).favorites
-    @recommendations = RecommendationEngine.new(favorite_movies).recommendations
-    render json: @recommendations
+    begin
+      user = User.find(params[:user_id])
+
+      favorite_movies = User.find(params[:user_id]).favorites
+      @recommendations = RecommendationEngine.new(favorite_movies).recommendations
+      render json: @recommendations
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { error: "User not found." }, status: :not_found
+    end
   end
 
   def user_rented_movies
-    @rented = User.find(params[:user_id]).rented
-    render json: @rented
+    begin
+      user = User.find(params[:user_id])
+
+      @rented = User.find(params[:user_id]).rented
+      render json: @rented
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { error: "User not found." }, status: :not_found
+    end
   end
 
   def rent
-    user = User.find(params[:user_id])
-    movie = Movie.find(params[:id])
-    movie.available_copies -= 1
-    movie.save
-    user.rented << movie
-    render json: movie
+    begin
+      user = User.find(params[:user_id])
+      movie = Movie.find(params[:id])
+
+      if movie.available_copies > 0
+        if Rental.exists?(user_id: user.id, movie_id: movie.id)
+          render json: { error: "This movie is already rented by the user." }, status: :unprocessable_entity
+        else
+          movie.available_copies -= 1
+          movie.save
+          user.rented << movie
+          render json: movie
+        end
+      else
+        render json: { error: "No available copies for rental." }, status: :unprocessable_entity
+      end
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { error: "User or movie not found." }, status: :not_found
+    end
   end
+
 end
